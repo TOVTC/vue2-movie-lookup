2ef63d3c81f35c738337df7f5d2e6ef3eb5b6c1f
TOVTC <veronica.tc.to@outlook.com>
2023-04-10 13:35:31 -0400

    converted common html into app.vue template

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,32 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    <header>
+          <router-link to="/"><h1>Movie Lookup</h1></router-link>
+        <nav>
+          <router-link to="/about">About</router-link> |
+          <router-link to="/trending">Trending</router-link> |
+          <router-link to="/popular">Popular</router-link> |
+          <router-link to="/top-rated">Top Rated</router-link> |
+          <router-link to="/now-playing">Now Playing</router-link>
+        </nav>
+    </header>
     <router-view/>
   </div>
 </template>
 
 <style>
+main {
+    margin: 5rem 10rem;
+}
+
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+    margin: 1.5rem;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -21,12 +39,13 @@ nav {
   padding: 30px;
 }
 
-nav a {
+header a, nav a {
+  text-decoration: none;
   font-weight: bold;
-  color: #2c3e50;
+  color: #000000;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: #808080;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section id="homepage">
+    <form id="search-form">
+        <label for="movie-name">Enter a movie name:</label><br/>
+        <input type="text" id="search" name="movie-name">
+        <button><a href="./results.html">Search</a></button>
+    </form>
+  </section>
 </template>
 
 <script>
