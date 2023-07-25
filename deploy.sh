b55bc66179eb447e7a237c148ed77f533986989b
Veronica Tai Chi To <99231049+TOVTC@users.noreply.github.com>
2023-07-25 18:52:17 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:TOVTC/vue-movie-lookup.git master:gh-pages
+git push -f git@github.com:TOVTC/vue2-movie-lookup.git master:gh-pages
 
 cd -
